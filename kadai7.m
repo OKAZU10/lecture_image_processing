a59cba7e0e723cfff61e296def65a668979bb2df
Kazuto Furusato <13ec090@ms.dendai.ac.jp>
2016-01-18 21:51:43 +0900

    画像入力方法変更

---
+++ kadai7.m
@@ -0,0 +1,22 @@
+% 課題7　ダイナミックレンジの拡大
+% 画素のダイナミックレンジを０から２５５にせよ． 
+% 下記はサンプルプログラムである． 
+% 課題作成にあたっては「Lenna」以外の画像を用いよ． 
+% 例
+
+clear; % 変数のオールクリア
+fileName = uigetfile('*');
+ORG  = imread(fileName); % 原画像の入力
+ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
+imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+pause;
+imhist(ORG); % 濃度ヒストグラムを生成、表示
+pause;
+ORG = double(ORG);
+mn = min(ORG(:)); % 濃度値の最小値を算出
+mx = max(ORG(:)); % 濃度値の最大値を算出
+ORG = (ORG-mn)/(mx-mn)*255;
+imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+pause;
+ORG = uint8(ORG); % この行について考察せよ
+imhist(ORG); % 濃度ヒストグラムを生成、表示
